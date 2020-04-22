4086cb58707f9844cbb3197eb30ddc3ecb59bcbe
kelvin dizon <kidswoosh@icloud.com>
2020-04-21 19:15:57 -0700

    Update gotIt.js

--- js/gotIt.js
+++ js/gotIt.js
@@ -7,14 +7,14 @@ function gotData(data) {
   fbData = data.val();
 
   if (fbData) { // check to see if there is something in your database to start
-     console.log('received data:');
-     console.log(fbData);
+    console.log('received data:');
+    console.log(fbData);
 
-     // create an array of the post values (if you need to loop through it retaining order of entries)
-     fbDataArray = Object.values(fbData);
-   } else {
-     console.log('nothing in this folder yet');
-   }
+    // create an array of the post values (if you need to loop through it retaining order of entries)
+    fbDataArray = Object.values(fbData);
+  } else {
+    console.log('nothing in this folder yet');
+  }
 
 
 }
@@ -28,12 +28,25 @@ function errData(err) {
 // the node folder name, id, and object are all passed in as parameters
 
 function createNode(_nodeFolder, _nodeId, _nodeObject) {
-firebase.database().ref(_nodeFolder + '/' + _nodeId).set(_nodeObject);
-
-// call this function to create and seed the folder!
-// createNode(folderName, "seed", {text: "this is to seed folder"});
-// (to test you can just paste it into the web console)
-
+  firebase.database().ref(_nodeFolder + '/' + _nodeId).set(_nodeObject );
 }
 
 // createNode(folderName, "test", {text:"hello"});
+
+
+// the update method will update an existing node
+
+function updateNode(_nodeFolder, _nodeID, _updateObject) {
+firebase.database().ref(_nodeFolder + '/' + _nodeId).update(_updateObject);
+// this will update existing key:value pair(s) OR add new ones to your object
+// so your object might look like:
+// { existingKey: updatedKeyValue,
+//   newKey: newValue }
+// Where the existing key is updated and newKey is added
+}
+
+// And this removes an entire node from your folder
+
+function deleteNode(_nodeFolder, _nodeID) {
+firebase.database().ref(_nodeFolder + '/' + _nodeID).remove();
+}
